9b3f1f8ecffef37483f8574e0a6735c33474d5f9
Sergio Usma <sergioandresusma@hotmail.com>
2024-02-09 16:32:07 -0500

    Connect API to fetch quotes & fill fields

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
 const copyQuoteBtn = document.getElementById('copyQuote');
+const newQuoteBtn = document.getElementById('newQuote');
 const genQuote = document.querySelector('.genQuote');
 const authorQuote = document.querySelector('.authorQuote');
 const copyAlert = document.getElementById('alertCopy');
-const fullQuoteAuthor = `${genQuote.textContent} - ${authorQuote.textContent}`;
+
+const API_URL = 'https://api.quotable.io/random';
+
+fetch(API_URL)
+  .then((response) => response.json())
+  .then((data) => {
+    genQuote.textContent = `"${data.content}"`;
+    authorQuote.textContent = data.author;
+  });
 
 function toClipboard(element) {
-  navigator.clipboard.writeText(element)
+  navigator.clipboard
+    .writeText(element)
     .then(() => {
       copyAlert.textContent = 'Copied!';
       copyAlert.classList.remove('hidden');
@@ -24,4 +34,10 @@ function toClipboard(element) {
     });
 }
 
-copyQuoteBtn.addEventListener('click', () => toClipboard(fullQuoteAuthor));
+copyQuoteBtn.addEventListener('click', () => {
+  toClipboard(`${genQuote.textContent} - ${authorQuote.textContent}`);
+});
+newQuoteBtn.addEventListener('click', () => {
+  // eslint-disable-next-line no-restricted-globals
+  location.reload();
+});
